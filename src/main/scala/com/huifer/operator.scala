25906a06ba5f068c707d960381ea055af9759807
huifer <huifer97@163.com>
2019-09-17 12:12:41 +0800

    操作符

---
+++ src/main/scala/com/huifer/operator.scala
@@ -0,0 +1,13 @@
+package com.huifer
+
+object operator {
+  def main(args: Array[String]): Unit = {
+    val inclusive = 1 to 10
+    val inclusive1 = 1.to(10)
+
+    val tuple = 1 -> 10
+    val tuple1 = 1.->(10)
+
+    var s = "2"
+  }
+}
